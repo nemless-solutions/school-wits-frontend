fe72e03035b9da3e6e40398dc683446c8839a867
SwagatoSarowar <swagotosarowar1@gmail.com>
2025-05-20 00:37:24 +0600

    homepage adjustments

--- apps/student-portal/src/components/Hero/Hero.tsx
+++ apps/student-portal/src/components/Hero/Hero.tsx
@@ -4,24 +4,30 @@ import Link from "next/link";
 
 export function Hero() {
   return (
-    <section className="min-h-screen -mt-20 flex items-center bg-radial-[at_80%_55%] from-blue-50 to-blue-200 to-75%">
+    <section className="min-h-screen md:-mt-20 flex items-center bg-radial-[at_80%_55%] from-blue-50 to-blue-200 to-75%">
       <div className="main-container w-full">
-        <div className="grid grid-cols-2">
-          <div className="text-5xl font-semibold flex flex-col justify-center gap-16">
-            <h1 className="text-secondary">
-              Let&apos;s Make Studying <br />
-              <span className="text-primary"> Less &apos;Meh&apos;</span>
-            </h1>
-            <div className="flex flex-col items-start gap-y-4">
-              <Button size="lg" asChild variant="secondary" className="text-lg">
-                <Link href="/">Start Your Learning Journey</Link>
-              </Button>
-              <Button size="lg" asChild className="text-lg">
-                <Link href="/">Browse Courses</Link>
-              </Button>
+        <div className="grid md:grid-cols-2 gap-y-4 justify-center">
+          <div className="text-3xl sm:text-4xl md:text-5xl text-center md:text-start font-semibold flex flex-col justify-center items-center md:items-start gap-10">
+            <div>
+              <h1 className="text-secondary">
+                Let&apos;s Make Studying <br />
+                <span className="text-primary"> Less &apos;Meh&apos;</span>
+              </h1>
+              <p className="text-secondary/80 text-base md:text-lg mt-4 max-w-[400px]">
+                Because education shouldn&apos;t feel like a chore. Dive into
+                courses designed to spark thinking and actually make sense.
+              </p>
             </div>
+            <Button
+              size="lg"
+              asChild
+              variant="outline"
+              className="text-sm md:text-lg w-fit h-[45px] md:h-[55px] shadow-[-3px_3px_5px_rgba(0,0,0,0.2)] hover:shadow-[-6px_6px_5px_rgba(0,0,0,0.3)] hover:bg-background duration-150 hover:-translate-y-0.5 font-semibold"
+            >
+              <Link href="/">Start Your Learning Journey</Link>
+            </Button>
           </div>
-          <div className="mt-20">
+          <div className="md:mt-20">
             <Image
               className="w-full object-cover drop-shadow-[-10px_10px_10px_rgba(0,0,0,0.4)]"
               src="/images/hero-graphics.png"
---
+++ apps/student-portal/src/components/Navbar/NavMenu.tsx
@@ -0,0 +1,57 @@
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+} from "@school-wits/ui";
+import Image from "next/image";
+import Link from "next/link";
+import { NavItem } from "./Navbar";
+
+interface NavMenuProps {
+  title: string;
+  content: NavItem[];
+}
+
+export function NavMenu({ title, content }: NavMenuProps) {
+  return (
+    <NavigationMenu>
+      <NavigationMenuList>
+        <NavigationMenuItem>
+          <NavigationMenuTrigger className="font-semibold text-base text-secondary !bg-transparent h-full py-5 focus:!bg-transparent hover:!bg-transparent w-full ml-[45px] md:ml-0 pr-[150px] md:pr-0">
+            {title}
+          </NavigationMenuTrigger>
+          <NavigationMenuContent>
+            <ul className="w-[200px] font-semibold">
+              {content.map((item, index) => (
+                <li key={index}>
+                  <NavigationMenuLink asChild>
+                    <Link href={item?.link || "/"}>
+                      <div className="flex h-full gap-x-2 w-full select-none items-center rounded-md bg-gradient-to-b from-muted/50 to-muted px-6 py-1 no-underline outline-none focus:shadow-md text-secondary">
+                        <Image
+                          className="block w-10 object-cover"
+                          src={item?.icon || ""}
+                          width={50}
+                          height={50}
+                          alt={item.title}
+                        />
+                        <span className="block">
+                          {item.title.split(" ")[0]}{" "}
+                          <span className="font-roboto-slab text-primary">
+                            {item.title.split(" ")[1]}
+                          </span>
+                        </span>
+                      </div>
+                    </Link>
+                  </NavigationMenuLink>
+                </li>
+              ))}
+            </ul>
+          </NavigationMenuContent>
+        </NavigationMenuItem>
+      </NavigationMenuList>
+    </NavigationMenu>
+  );
+}
