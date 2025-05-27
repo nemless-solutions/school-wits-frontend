ba8b8e0d59c8bfa31b382b8a05d4dcfc00981dd2
SwagatoSarowar <swagotosarowar1@gmail.com>
2025-05-27 18:14:15 +0600

    configured for pm2

--- apps/student-portal/next.config.js
+++ apps/student-portal/next.config.js
@@ -6,7 +6,6 @@ const { composePlugins, withNx } = require("@nx/next");
  * @type {import('@nx/next/plugins/with-nx').WithNxOptions}
  **/
 const nextConfig = {
-  distDir: "../../dist/apps/student-portal",
   // Use this to set Nx-specific options
   // See: https://nx.dev/recipes/next/next-config-setup
   nx: {},
