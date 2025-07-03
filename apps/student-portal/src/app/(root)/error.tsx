dda39a2528f477b57fdbc9a7d7a6e77aeac67029
SwagatoSarowar <swagotosarowar1@gmail.com>
2025-07-03 23:44:01 +0600

    edited erros page

--- apps/student-portal/src/app/(root)/error.tsx
+++ apps/student-portal/src/app/(root)/error.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { Button } from "@/components/client-ui";
+import { Button, MotionDiv } from "@/components/client-ui";
 import { PageHeader } from "@/components/PageHeader/PageHeader";
 import { useEffect } from "react";
 
@@ -16,21 +16,48 @@ export default function GlobalError({
   }, [error]);
 
   return (
-    <div>
+    <>
       <PageHeader header="Error" />
-      <div className="main-container text-center">
-        <div className="py-16">
-          <h2 className="text-[28px] sm:text-4xl md:text-[54px] font-semibold text-destructive capitalize font-recoleta">
-            Ooops!
-          </h2>
-          <p className="md:text-lg font-semibold my-6">
-            {error.message || "Something went wrong."}
-          </p>
-          <Button variant="secondary" onClick={() => reset()}>
-            Try again
-          </Button>
+      <div className="relative overflow-clip">
+        <div className="absolute top-1/3 -right-20 w-[400px] md:w-xl aspect-square bg-secondary/15 rounded-full blur-[100px]"></div>
+        <div className="absolute top-1/3 -left-20 -translate-y-1/2 w-xs aspect-square bg-primary/35 rounded-full blur-[120px]"></div>
+        <div className="main-container relative z-10">
+          <div className="text-center space-y-4 py-16">
+            <MotionDiv
+              initial={{ opacity: 0, x: -20 }}
+              animate={{ opacity: 1, x: 0 }}
+              transition={{ duration: 0.5, easings: "easeInOut", delay: 0.3 }}
+            >
+              <h2 className="text-4xl sm:text-6xl md:text-9xl font-bold text-destructive capitalize font-recoleta">
+                500
+              </h2>
+            </MotionDiv>
+            <MotionDiv
+              initial={{ opacity: 0, x: -20 }}
+              animate={{ opacity: 1, x: 0 }}
+              transition={{ duration: 0.5, easings: "easeInOut", delay: 0.6 }}
+            >
+              <p className="text-lg md:text-xl font-semibold capitalize text-destructive mb-8">
+                Ooops! Seems like something went wrong.
+              </p>
+            </MotionDiv>
+            <MotionDiv
+              initial={{ opacity: 0, x: -20 }}
+              animate={{ opacity: 1, x: 0 }}
+              transition={{ duration: 0.5, easings: "easeInOut", delay: 0.9 }}
+            >
+              <Button
+                variant="secondary"
+                size="lg"
+                className="text-base md:text-lg font-semibold"
+                onClick={() => reset()}
+              >
+                Retry
+              </Button>
+            </MotionDiv>
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
